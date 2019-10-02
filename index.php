dc48f398cbaf546447f9a31f57b1f7f7d5a0eb0e
ahmadant <31107866+ahmadant@users.noreply.github.com>
2019-10-02 12:11:16 +0700

    kurang besar penulisan

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php include "header.php"; ?>
 <div class="jumbotron text-center">
-	<h3>Management Kamar Pondok</h3>
-	<h2><strong>KAMAR PONDOK</strong></h2>
+	<h3>Management Kamar Pondok (Wonosobo)</h3>
+	<h3><strong>KAMAR PONDOK</strong></h3>
 </div>
-
 <?php include "footer.php"; ?>
